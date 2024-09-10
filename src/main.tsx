1cb27070612b8ede7f3b1fc6172f3c3304df2871
MRyabushkin <mikhail.ryabushkin@mediadirectiongroup.ru>
2024-09-10 12:58:19 +0300

    linter: formatting all files

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+
+import { MantineProvider } from '@mantine/core';
 
 import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
@@ -8,7 +8,9 @@ import '@mantine/dropzone/styles.css';
 import '@mantine/notifications/styles.css';
 import '@mantine/spotlight/styles.css';
 import '@mantine/tiptap/styles.css';
-import { MantineProvider } from '@mantine/core';
+
+import App from './App.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <MantineProvider>
